6325a80b6e743ceac2f99ffbef82d837204e18b7
pujol99 <alexpujolcomet@gmail.com>
2020-07-30 20:53:29 +0200

    update structure

---
+++ src/main/java/Classes/Room.java
@@ -0,0 +1,28 @@
+package Classes;
+
+public class Room {
+
+    //Variables
+    private int capacity;
+    private int roomNumber;
+    private boolean occupied;
+    /*
+        Instead of a boolean use a structure to know which days will be occupied
+     */
+
+    //Constructor
+    public Room(){}
+
+    public Room(int capacity, int roomNumber){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+    }
+
+    //Functions
+    //public void bookRoom
+
+    //Getters/Setters
+
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Calendar myCalendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Madrid"));
+        Date time = myCalendar.getTime();
+
     }
 }
